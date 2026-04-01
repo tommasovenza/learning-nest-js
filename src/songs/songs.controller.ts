7c5d71ed45593b0146a2eb4fba2c17f57da19520
tommasovenza <tommasovenza@gmail.com>
2026-04-01 12:30:24 +0200

    add songs controller and update prettier config

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common'
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+  // Post
+  @Post()
+  // Create Method
+  create() {
+    return 'POST => create a new song endpoint'
+  }
+
+  @Get()
+  // findAll Method
+  findAll() {
+    return 'GET => find all songs endpoint'
+  }
+
+  @Get(':id')
+  // findOne
+  findOne() {
+    return 'GET => fetch song on the based on id'
+  }
+
+  @Put(':id')
+  // update
+  update() {
+    return 'PUT => update song on the based on id'
+  }
+
+  @Delete(':id')
+  // delete
+  delete() {
+    return 'DELETE => delete a song on the based on id'
+  }
+}
